388e1fa961c797e381e345f1adb7f2badc7859c3
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-10-22 18:32:12 +0300

    started create homepage and paths config

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import paths from "./paths/pathsConfig";
 function App() {
-  return <div className="App">sup mate</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {paths.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            element={<route.element />}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/paths/pathsConfig.js
@@ -0,0 +1,5 @@
+import { HomePage } from "../pages/HomePage/HomePage";
+
+const paths = [{ path: "/", exact: true, element: HomePage }];
+
+export default paths;
