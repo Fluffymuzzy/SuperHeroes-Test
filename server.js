22841ccdf024e1636d28b39a9d47988c9f09dbc3
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-10-22 14:14:08 +0300

    create based setup for server *-*

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+/* A middleware that parses the body of the request and makes it available on the request object. */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/static", express.static(__dirname + "/assets"));
+
+app.get("/api/superheroes", require(".routes/superheroes"));
+
+mongoose.connect("mongodb://localhost:27017").then(() => {
+  app.listen(port, () => {
+    console.log(`god bless it's working on ${port}`);
+  });
+});
