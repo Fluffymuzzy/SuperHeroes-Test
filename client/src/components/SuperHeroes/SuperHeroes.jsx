1cfecd273e12c1c72d226a899b26178a00b3e926
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-10-23 13:53:24 +0300

    added spinner

--- client/src/components/SuperHeroes/SuperHeroes.jsx
+++ client/src/components/SuperHeroes/SuperHeroes.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSuperHeroes } from "../../store/superheroesSlice/superheroesSlice";
+import Spinner from "../Spinner/Spinner";
+
 const SuperHeroes = () => {
   const dispatch = useDispatch();
   const { superheroes, isLoading } = useSelector((state) => state.superheroes);
@@ -9,6 +11,11 @@ const SuperHeroes = () => {
     dispatch(getSuperHeroes());
   }, [dispatch]);
 
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  
   return <div>SuperHeroes</div>;
 };
 
