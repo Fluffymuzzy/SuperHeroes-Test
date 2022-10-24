a00e94ccb167d87a44f04b3a52644c9940869c3f
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2022-10-24 16:13:56 +0300

    corrections :angel:

--- client/src/pages/HeroPage/HeroPage.jsx
+++ client/src/pages/HeroPage/HeroPage.jsx
@@ -19,45 +19,47 @@ const HeroPage = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.img__container}>
-          <img
-            className={styles.image}
-            src={superhero.images}
-            alt={superhero.nickname}
-          />
-        </div>
-        <div className={styles.info}>
-          <h1 className={styles.nickname}>{superhero.nickname}</h1>
-          <li className={styles.list__item}>
-            <span className={styles.item__title}>
-              real name: {superhero.real_name}
-            </span>
-          </li>
-          <li className={styles.list__item}>
-            <span className={styles.item__title}>
-              superpowers: {superhero.superpowers}
-            </span>
-          </li>
-          <li className={styles.list__item}>
-            <span className={styles.item__title}>
-              catch phrase: {superhero.catch_phrase}
-            </span>
-          </li>
-          <li className={styles.list__item}>
-            <span className={styles.item__title}>
-              origin: {superhero.origin_description}
-            </span>
-          </li>
-          <div className={styles.itemBtns}>
-            <button onClick={() => navigate(-1)}>back</button>
-            <button onClick={() => navigate("/")}>edit</button>
-            <button onClick={() => navigate("/")}>delete</button>
+    <>
+      <button onClick={() => navigate(-1)}>back</button>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.img__container}>
+            <img
+              className={styles.image}
+              src={superhero.images}
+              alt={superhero.nickname}
+            />
+          </div>
+          <div className={styles.info}>
+            <h1 className={styles.nickname}>{superhero.nickname}</h1>
+            <li className={styles.list__item}>
+              <span className={styles.item__title}>
+                real name: {superhero.real_name}
+              </span>
+            </li>
+            <li className={styles.list__item}>
+              <span className={styles.item__title}>
+                superpowers: {superhero.superpowers}
+              </span>
+            </li>
+            <li className={styles.list__item}>
+              <span className={styles.item__title}>
+                catch phrase: {superhero.catch_phrase}
+              </span>
+            </li>
+            <li className={styles.list__item}>
+              <span className={styles.item__title}>
+                origin: {superhero.origin_description}
+              </span>
+            </li>
+            <div className={styles.itemBtns}>
+              <button onClick={() => navigate("/")}>edit</button>
+              <button onClick={() => navigate("/")}>delete</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
